bf22be2fc2775f51b06526aa35887b54c6a4161d
gsn1417 <gsn1417@qq.com>
2026-07-07 15:40:59 +0800

    feat: rm update mod

--- cp_mods.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-# set -o xtrace
-SCRIPT_PATH="`dirname \"$0\"`"
-
-pushd ${SCRIPT_PATH}
-
-cp dedicated_server_mods_setup.lua ../../../mods/dedicated_server_mods_setup.lua.bak
-
-popd
